88647438e864d4c55833b1608377e7907a3691f7
Narek Sahakyan <narek.sv.work@gmail.com>
2022-08-19 02:37:38 +0400

    Documentation improvements

--- Sources/KeyValueStorage/KeyValueStorageKey.swift
+++ Sources/KeyValueStorage/KeyValueStorageKey.swift
@@ -5,11 +5,21 @@
 //  Created by Narek Sahakyan on 7/27/22.
 //
 
+/// This struct is intended to uniquely identify the item value, type, and all the necessary info about the storage
 public struct KeyValueStorageKey<T: Codable> {
+    
+    /// `name` is used to uniquely identify the item
     let name: String
+    
+    /// `codingType` is used for properly encoding and decoding the item
     let codingType: T.Type
+    
+    /// `storageType` is used for specifing the storage type where the item will be kept
     let storageType: KeyValueStorageType
     
+    /// Initializes the key by specifying the key name and the storage type
+    /// - parameter name: The name of the key
+    /// - parameter storage: The storage type. Default value is `userDefaults`
     init(name: String, storage: KeyValueStorageType = .userDefaults) {
         self.name = name
         self.codingType = T.self
--- Sources/KeyValueStorage/KeyValueStorageType.swift
+++ Sources/KeyValueStorage/KeyValueStorageType.swift
@@ -5,8 +5,17 @@
 //  Created by Narek Sahakyan on 7/27/22.
 //
 
+/// This enum contains all the supported storage types
 public enum KeyValueStorageType: Equatable {
+    
+    /// This storage type persists only within an app session
     case inMemory
+    
+    /// This storage type persists within the whole app lifetime
     case userDefaults
+    
+    /// This storage type keeps the items in a secure storage and persists even app re-installations
+    /// - parameter accessibility: Accessibility to use when retrieving the keychain item.
+    /// - parameter isSynchronizable: A bool that describes if the item should be synchronizable, to be synched with the iCloud.
     case keychain(accessibility: KeychainAccessibility = .whenUnlocked, isSynchronizable: Bool = false)
 }
